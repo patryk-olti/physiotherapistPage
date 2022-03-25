3d70e55502514627a899e64778ae2b54f4a83c0a
Patryk Oltuch <95oltuchpatryk@gmail.com>
2022-03-25 18:27:04 +0100

    finished contact section

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 import styles from '../styles/index.module.css';
+import custom from '../styles/customs.module.css';
 
 const Contact = () => {
 
+    const sectionRef = useRef();
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    useEffect( () => {
+        gsap.fromTo( sectionRef.current, { y: '+=100', alpha: 0}, { y: 0, alpha: 1, duration: 1.5, scrollTrigger: {
+            trigger: '#contact',
+            start: 'top 40%',
+            end: 'top top',
+            scrub: 1
+        }})
+    }, [])
+
     return(
         <div className={styles.container} id='contact'>
-            <div className={styles.content__flexColumn}>
-                Contact
+            <div className={styles.content__flexColumn} ref={sectionRef}>
+                <div className={styles.content__flexRow}>
+                    <div className={custom.tile} >
+                        <a href="#" className={styles.iconLarge}> <FontAwesomeIcon icon={faLocationDot} /> </a>
+                        <h1> Wrocław </h1>
+                        <h2> ul. Najnowsza 35/2 </h2>
+                    </div>
+
+                    <div className={custom.tile}>
+                        <a href="#" className={styles.iconLarge}> <FontAwesomeIcon icon={faPhone} /> </a>
+                        <h1> telefon </h1>
+                        <h2> 123-456-789 </h2>
+                    </div>
+
+                    <div className={custom.tile}>
+                        <a href="#" className={styles.iconLarge}> <FontAwesomeIcon icon={faEnvelope} /> </a>
+                        <h1> e-mail </h1>
+                        <h2> przyklad@gmail.com </h2>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/sections/Offerts.js
+++ src/sections/Offerts.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useContext } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import gsap from 'gsap';
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
