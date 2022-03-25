b54662c54f1505949f3caa380bc0165a7ae69f1d
Patryk Oltuch <95oltuchpatryk@gmail.com>
2022-03-25 22:03:33 +0100

    fixed offerts section

--- src/sections/Offerts.js
+++ src/sections/Offerts.js
@@ -40,15 +40,22 @@ const Offerts = () => {
 
                 <div className={styles.content__flexColumn}>
                     <h1> Technika Nr. 2 </h1>
+
                     <div className={styles.content}> 
                         <section className={styles.textArea}>
-                            <img src={imgStones} alt='img' className={styles.imgMedium} />
+                            <img src={imgStones} alt='img' className={styles.imgMediumRow} />
                         </section>
 
                         <section className={styles.textArea}>
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                         </section>
+
+                        <section className={styles.textArea}>
+                            <img src={imgStones} alt='img' className={styles.imgMediumColumn} />
+                        </section>
                     </div>
+
+
                 </div>
 
                 <div className={styles.content__flexColumn}>
