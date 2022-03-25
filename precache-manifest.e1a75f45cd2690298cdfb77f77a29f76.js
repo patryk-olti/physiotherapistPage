f001e228c672f555c4292f30fea8c72823b858b3
Patryk Oltuch <95oltuchpatryk@gmail.com>
2022-03-25 21:29:53 +0100

    Updates

---
+++ precache-manifest.e1a75f45cd2690298cdfb77f77a29f76.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4eb9b561050de082b691",
+    "url": "/physiotherapistPage/static/css/main.4b6e75f7.chunk.css"
+  },
+  {
+    "revision": "4eb9b561050de082b691",
+    "url": "/physiotherapistPage/static/js/main.4eb9b561.chunk.js"
+  },
+  {
+    "revision": "845415a64ad85c2ab603",
+    "url": "/physiotherapistPage/static/js/1.845415a6.chunk.js"
+  },
+  {
+    "revision": "71a617584aca29969036",
+    "url": "/physiotherapistPage/static/js/runtime~main.71a61758.js"
+  },
+  {
+    "revision": "153b10562b5cdab212289e89827957be",
+    "url": "/physiotherapistPage/static/media/kamienie.153b1056.jpg"
+  },
+  {
+    "revision": "8e8d49e38e7d7983364397705b185f91",
+    "url": "/physiotherapistPage/static/media/person_background.8e8d49e3.jpg"
+  },
+  {
+    "revision": "0c7f52727f75c08fbf401fe847e077d2",
+    "url": "/physiotherapistPage/static/media/salon.0c7f5272.jpg"
+  },
+  {
+    "revision": "cff5174f67875e2fbc8cf49008634169",
+    "url": "/physiotherapistPage/index.html"
+  }
+];
